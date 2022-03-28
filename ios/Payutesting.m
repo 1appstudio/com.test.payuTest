a36420548a181f6716e62f9b64f811087dc01602
appstudio@icloud.com <jk@JKs-MacBook-Pro.local>
2022-03-28 12:32:06 +0530

    v2

--- ios/Payutesting.m
+++ ios/Payutesting.m
@@ -2,8 +2,10 @@
 
 @interface RCT_EXTERN_MODULE(Payutesting, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+//RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+//                 withResolver:(RCTPromiseResolveBlock)resolve
+//                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(start:(NSDictionary *)paymentDetails withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
 
 @end
