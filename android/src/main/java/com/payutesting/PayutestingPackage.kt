ff7485349ac8b9fba61681a30afeba7be6f36e84
appstudio@icloud.com <jk@JKs-MacBook-Pro.local>
2022-03-29 11:35:08 +0530

    android_fixed

--- android/src/main/java/com/payutesting/PayutestingPackage.kt
+++ android/src/main/java/com/payutesting/PayutestingPackage.kt
@@ -4,15 +4,12 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import com.payutesting.PayutestingModule
 
 
 class PayutestingPackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
         return listOf(PayutestingModule(reactContext))
     }
-
-
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
         return emptyList()
